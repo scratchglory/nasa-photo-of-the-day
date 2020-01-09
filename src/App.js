08f83970622c14bdc6845b21af3a803e408f21e8
Adella <scratchglory@gmail.com>
2020-01-08 22:05:06 -0500

    added reactstrap and styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import NasaPhotoInfo from "./components/NasaPhotoInfo";
+import styled, { css } from "styled-components";
 
 import "./App.css";
+
+const Header = styled.h1`
+  margin-top: 2%;
+  font-family: "Cute Font", cursive;
+  font-size: 4.3rem;
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>🚀NASA's Astronomy Picture of the Day!✨</h1>
+      <Header>🚀NASA's Astronomy Polaroid of the Day!✨</Header>
       <NasaPhotoInfo />
     </div>
   );
--- src/components/NasaPhotoInfo.js
+++ src/components/NasaPhotoInfo.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle
+} from "reactstrap";
+import styled, { css } from "styled-components";
+
+const CardInfo = styled.div`
+  width: 75%;
+  border: 2px dashed black;
+  box-shadow: 20px 20px 8px #888888;
+  margin: 2% auto;
+`;
+
+const Title = styled.h2`
+  margin: 1% 0;
+  color: #4169e1;
+  text-decoration: underline;
+  font-family: "Indie Flower", cursive;
+`;
 
 export default function NasaPhoto() {
   const [photo, setPhoto] = useState([]);
-
+  //   const ApiKey = Ha6eDiYXL3et4IsPieJpbAGMSPf8fxS8RkynwVRW;
   useEffect(() => {
     axios
       .get(
@@ -14,16 +37,18 @@ export default function NasaPhoto() {
         console.log("NO DATA", err);
       });
   }, []);
+
   console.log(photo);
 
   return (
-    <div className="img-of-day">
-      <h2>{photo.title}</h2>
-      <h4>{photo.date}</h4>
-
-      <img src={photo.url} height="600px" width="75%" />
-      <p>Credit: {photo.copyright}</p>
-      <div className="explanation">{photo.explanation}</div>
-    </div>
+    <CardInfo className="img-of-day">
+      <CardBody>
+        <CardImg height="600px" top width="400px" src={photo.url} />
+        <Title>{photo.title}</Title>
+        <CardSubtitle>{photo.date}</CardSubtitle>
+        <CardSubtitle>Credit: {photo.copyright}</CardSubtitle>
+        <CardText className="explanation">{photo.explanation}</CardText>
+      </CardBody>
+    </CardInfo>
   );
 }
